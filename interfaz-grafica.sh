127e5b02a888b7fe1fee9f3abbbd0b5b3d695801
Agustin Ranieri <aguseranieri@gmail.com>
2022-05-28 14:07:25 -0300

    Update interfaz-grafica.sh

--- interfaz-grafica.sh
+++ interfaz-grafica.sh
@@ -35,7 +35,5 @@ sudo apt install code
 
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.hexeditor
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension MS-vsliveshare.vsliveshare
 code --install-extension eamodio.gitlens
 code --install-extension nhoizey.gremlins
