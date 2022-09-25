86d4ab470ebe987dcb5b28668aebb64b2ae8790a
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-25 16:22:14 -0300

    fix: remove warning after upgrading gdb

--- base-server.sh
+++ base-server.sh
@@ -44,9 +44,10 @@ vim \
 emacs
 
 ARCHITECTURE=$(dpkg --print-architecture)
-wget "https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/ppa/+build/23606523/+files/gdb_12.1-0ubuntu1~22.04_${ARCHITECTURE:?}.deb"
-sudo apt-get install -y ./gdb_12.1-0ubuntu1~22.04_${ARCHITECTURE:?}.deb
-rm -f ./gdb_12.1-0ubuntu1~22.04_${ARCHITECTURE:?}.deb
+sudo wget -O "/tmp/gdb_12.1-0ubuntu1~22.04_${ARCHITECTURE:?}.deb" \
+  "https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/ppa/+build/23606523/+files/gdb_12.1-0ubuntu1~22.04_${ARCHITECTURE:?}.deb"
+sudo apt-get install -yf "/tmp/gdb_12.1-0ubuntu1~22.04_${ARCHITECTURE:?}.deb"
+sudo rm -f "/tmp/gdb_12.1-0ubuntu1~22.04_${ARCHITECTURE:?}.deb"
 
 sudo ssh-keygen -A
 sudo /etc/init.d/ssh start
