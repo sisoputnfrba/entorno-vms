3596357c1d6f1c6f12903330b2f51e81f0668e09
Agustin Ranieri <aguseranieri@gmail.com>
2024-04-10 16:27:40 -0300

    feat: install code from snap package

--- interfaz-grafica.sh
+++ interfaz-grafica.sh
@@ -49,12 +49,7 @@ StartupNotify=false
 EOF
 
 # Se instala VSCode siguiendo los requisitos de Microsoft
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt-get update -y
-sudo apt-get install -y code
+sudo snap install code --classic
 
 # Se instalan las extensiones recomendadas
 code --install-extension ms-vscode.cpptools-extension-pack
