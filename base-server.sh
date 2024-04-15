14a9cfcd15fc681153253183be57680783de42a8
Agustin Ranieri <aguseranieri@gmail.com>
2024-04-15 12:08:56 -0300

    Feat: Agrego clang-format

--- base-server.sh
+++ base-server.sh
@@ -4,9 +4,19 @@ set -e
 
 apt-get update -y && apt-get -y install software-properties-common
 
+curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | gpg --dearmor --yes -o /etc/apt/keyrings/llvm-snapshot.gpg
+
+ARCHITECTURE=$(dpkg --print-architecture)
+LSB_RELEASE=$(lsb_release -cs)
+
+cat > /etc/apt/sources.list.d/llvm.list <<EOF
+deb [arch=${ARCHITECTURE:?} signed-by=/etc/apt/keyrings/llvm-snapshot.gpg] http://apt.llvm.org/${LSB_RELEASE:?}/ llvm-toolchain-${LSB_RELEASE:?} main
+deb-src [arch=${ARCHITECTURE:?} signed-by=/etc/apt/keyrings/llvm-snapshot.gpg] http://apt.llvm.org/${LSB_RELEASE:?}/ llvm-toolchain-${LSB_RELEASE:?} main
+EOF
+
 add-apt-repository -y ppa:git-core/ppa
 
-apt-get install -y \
+apt-get update -y && apt-get install -y \
 libbison-dev \
 libcunit1-dev \
 libevent-dev \
@@ -20,6 +30,7 @@ cmake \
 g++ \
 gcc \
 gdb \
+clang-format \
 ltrace \
 make \
 strace \
