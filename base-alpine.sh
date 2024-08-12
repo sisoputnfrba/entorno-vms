7761cff4ae44bdfa7334fe27285770054c51303a
Agustin Ranieri <aguseranieri@gmail.com>
2024-08-12 01:05:04 -0300

    Create base-alpine.sh

---
+++ base-alpine.sh
@@ -0,0 +1,36 @@
+#!/bin/ash -x
+
+# setup-alpine
+# reboot
+
+sed -i 's/#http/http/g' /etc/apk/repositories
+
+apk update
+apk add \
+  bash \
+  curl \
+  git \
+  tig \
+  htop \
+  xclip \
+  nano \
+  vim \
+  emacs \
+  build-base \
+  valgrind \
+  strace \
+  ltrace \
+  readline-dev \
+  ncurses-dev \
+  fuse-dev \
+  libevent-dev \
+  libuv-dev \
+  cunit-dev \
+  cmake \
+  automake \
+  autoconf \
+  procps-ng
+
+sed -i 's/AllowTcpForwarding no/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+
+rc-service sshd restart
