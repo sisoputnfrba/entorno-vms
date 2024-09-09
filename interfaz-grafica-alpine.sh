33cb3165f24d997b3b236fd507cb68bad16c627c
Agustin Ranieri <aguseranieri@gmail.com>
2024-09-09 01:04:25 -0300

    Update interfaz-grafica-alpine.sh

--- interfaz-grafica-alpine.sh
+++ interfaz-grafica-alpine.sh
@@ -1,8 +1,13 @@
 #!/bin/ash
 
-doas setup-xorg-base xfce4 lightdm-gtk-greeter terminator firefox mousepad
-
-doas apk add gvfs xfce4-screenshooter xarchiver
+doas setup-xorg-base xfce4 lightdm-gtk-greeter \
+  terminator \
+  firefox \
+  mousepad \
+  elogind polkit-elogind \
+  gvfs udisks2 \
+  xfce4-screenshooter \
+  xarchiver
 
 doas rc-update add lightdm
 doas rc-update add dbus
