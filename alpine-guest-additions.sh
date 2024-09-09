a1e450cf1b67c29b96150775a480507d665fae8d
Agustin Ranieri <aguseranieri@gmail.com>
2024-09-09 01:13:53 -0300

    Create alpine-guest-additions.sh

---
+++ alpine-guest-additions.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+apk add virtualbox-guest-additions virtualbox-guest-additions-x11
+
+rc-service virtualbox-guest-additions start
+rc-update add virtualbox-guest-additions boot
+
+rc-service virtualbox-drm-client start
+rc-update add virtualbox-drm-client default
